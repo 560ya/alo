61f7de54463d4b4d952585b83ca1e5d96e16c8f3
560ya <mirzaakbarovabdulazizxon@gmail.com>
2024-08-31 12:21:57 +0500

    izoh

--- ismoil/lib/main.dart
+++ ismoil/lib/main.dart
@@ -17,30 +17,62 @@ class bir extends StatefulWidget {
 }
 
 class _birState extends State<bir> {
- 
-  Color firstAvatarColor = Colors.black;
-  Color secondAvatarColor = Colors.black;
-  Color thirdAvatarColor = Colors.black;
+  // State to keep track of the selected star index
+  int selectedStar = 0;
 
-  int buttonPressCount = 0;
+  // State to keep track of the text value
+  int textCount = 1;
+  String topRightText = 'R1';
 
-  void _changeAvatarColor() {
+  // State to manage the visibility of the input field in the top right
+  bool isTopRightEditing = false;
+
+  // State to keep track of the center text
+  String centerText = "Nazarov O";
+
+  // State to show/hide the input field
+  bool isEditing = false;
+
+  // Controller to manage the input field text
+  TextEditingController _controller = TextEditingController();
+  TextEditingController _topRightController = TextEditingController();
+
+  // Function to change the selected star
+  void _onStarTap(int index) {
     setState(() {
-      buttonPressCount++;
+      selectedStar = index;
+    });
+  }
 
-      // Reset all colors to black
-      firstAvatarColor = Colors.black;
-      secondAvatarColor = Colors.black;
-      thirdAvatarColor = Colors.black;
+  // Function to update the top-right text value when tapped
+  void _toggleTopRightEditing() {
+    setState(() {
+      isTopRightEditing = true;
+      _topRightController.text = topRightText; // Set the current text in the input field
+    });
+  }
 
+  // Function to save the new top-right text value
+  void _saveTopRightText() {
+    setState(() {
+      topRightText = _topRightController.text;
+      isTopRightEditing = false;
+    });
+  }
 
-      if (buttonPressCount % 3 == 1) {
-        firstAvatarColor = Colors.red;
-      } else if (buttonPressCount % 3 == 2) {
-        secondAvatarColor = Colors.amber;
-      } else if (buttonPressCount % 3 == 0) {
-        thirdAvatarColor = Colors.green;
-      }
+  // Function to toggle between text and input field in the center
+  void _toggleEditing() {
+    setState(() {
+      isEditing = !isEditing;
+      _controller.text = centerText; // Set the current text in the input field
+    });
+  }
+
+  // Function to save the new center text value
+  void _saveText() {
+    setState(() {
+      centerText = _controller.text; // Update the center text with input value
+      isEditing = false; // Hide the input field
     });
   }
 
@@ -48,30 +80,107 @@ class _birState extends State<bir> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 70,
-              backgroundColor: firstAvatarColor,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            image: DecorationImage(
+              image: AssetImage("rasm/rasm17.jpg"),
+              fit: BoxFit.cover,
             ),
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 70,
-              backgroundColor: secondAvatarColor,
-            ),
-            SizedBox(height: 20), 
-            CircleAvatar(
-              radius: 70,
-              backgroundColor: thirdAvatarColor,
-            ),
-          ],
+          ),
+          height: 400,
+          width: 600,
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // Aligns stars to the left
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: List.generate(5, (index) {
+                      return IconButton(
+                        icon: Icon(
+                          Icons.star,
+                          color: index < selectedStar ? Colors.amber : Colors.grey,
+                        ),
+                        onPressed: () {
+                          _onStarTap(index + 1); // Update the selected star index
+                        },
+                      );
+                    }),
+                  ),
+                ],
+              ),
+              // Top-right text (R1, R2, ...)
+              Positioned(
+                top: 10,
+                right: 10,
+                child: isTopRightEditing
+                    ? Container(
+                        width: 80, // Small input field
+                        child: TextField(
+                          controller: _topRightController,
+                          onSubmitted: (_) => _saveTopRightText(),
+                          decoration: InputDecoration(
+                            hintText: "Enter text",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: _toggleTopRightEditing,
+                        child: Text(
+                          topRightText,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+              ),
+              // Bottom-right date text
+              Positioned(
+                bottom: 10, // Position the text at the bottom-right corner
+                right: 10,
+                child: Text(
+                  '31.07.2024',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // Center text (Nazarov O)
+              Center(
+                child: isEditing
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextField(
+                          controller: _controller,
+                          onSubmitted: (_) => _saveText(),
+                          decoration: InputDecoration(
+                            hintText: "Enter new text",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: _toggleEditing, // Toggle to input field on tap
+                        child: Text(
+                          centerText,
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _changeAvatarColor,
-        child: Icon(Icons.color_lens),
       ),
     );
   }
